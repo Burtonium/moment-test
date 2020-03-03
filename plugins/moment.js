464a07e88a4a746474b218ebb0a0b14c0c811915
Matt Burton <matt@elixxir.io>
2020-03-03 12:26:47 +0700

    moment config test

--- plugins/moment.js
+++ plugins/moment.js
@@ -1,5 +1,6 @@
 export default ({ app }) => {
   const { $moment } = app;
+  console.log(app.i18n.locale);
   $moment.locale(app.i18n.locale);
 
   app.i18n.onLanguageSwitched = (_oldLocale, newLocale) => {
